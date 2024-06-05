ef3fa96baab9a9435eec81f51d4ae628e8664f8a
Ludson Pereira <ludson_ps27@hotmail.com>
2024-06-05 17:22:45 -0300

    feat: :sparkles: add login service

--- 1-Curso de NestJS do Zero ao avançado/1-primeiro-contato/src/modules/login/services/login.service.ts
+++ 1-Curso de NestJS do Zero ao avançado/1-primeiro-contato/src/modules/login/services/login.service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInDto } from '../dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
-import { IUserRepository } from 'src/modules/user/repositories/user.repository';
+import { PrismaService } from 'src/infra/database/prisma.service';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class Login {
   constructor(
     private jwtService: JwtService,
-    private userService: IUserRepository,
+    private prisma: PrismaService,
   ) {}
 
-  async SignIn(data: SignInDto) {}
+  async SignIn(data: SignInDto) {
+    const user = await this.prisma.users.findFirst({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    // Validar a senha
+    const isEqualPassword = await compare(data.password, user.password);
+
+    if (!isEqualPassword) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
